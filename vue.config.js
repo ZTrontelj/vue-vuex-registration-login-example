52829526f9a6800c7328cbb85f1a793d4a0cec3b
Žiga Trontelj <svizec96@gmail.com>
2019-01-03 12:14:49 +0000

    revert changes

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-// vue.config.js file to be place in the root of your repository
-// make sure you update `yourProjectName` with the name of your GitLab project
-
-module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/yourProjectName/'
-    : '/'
-}
